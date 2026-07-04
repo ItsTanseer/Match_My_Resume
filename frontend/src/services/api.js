8e8d58b8983e62b9996bf4fbb79043c299e043d7
Tanseer <tanseer.ahmad.2007@gmail.com>
2026-07-04 18:48:30 +0530

    commited frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://match-my-resume-ofrw.onrender.com'
 })
 
 export const analyzeResume = (formData) => api.post('/analyze', formData)
